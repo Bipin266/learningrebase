34f4fa0148a2f0011ed93c65dc9d93dccce1bbc4
BipinKori <bipin.manikchand-kori@capgemini.com>
2025-02-22 21:27:02 +0530

    Initial setup

---
+++ WorkingWithStudent/src/advancejava/StreamAPI.java
@@ -0,0 +1,10 @@
+package advancejava;
+
+public class StreamAPI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ WorkingWithStudent/src/corejava/Operators.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class Operators {
+
+	
+	static {
+		System.out.println("Hello ");
+	}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
