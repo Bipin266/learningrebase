dcf5e72d6b95724602de58afa1f713628b17b97f
BipinKori <bipin.manikchand-kori@capgemini.com>
2025-02-22 21:32:50 +0530

    Even add to list

--- WorkingWithStudent/src/advancejava/StreamAPI.java
+++ WorkingWithStudent/src/advancejava/StreamAPI.java
@@ -1,10 +1,24 @@
 package advancejava;
 
+import java.util.List;
+
 public class StreamAPI {
+	List<Integer> num;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		StreamAPI start=new StreamAPI();
+		start.add(10);
 
 	}
+	
+	public void add(int number) {
+		for(int i=1;i<=number;i++) {
+			if(i%2==0) 
+			{num.add(i);}
+			
+		}
+	}
+	
 
 }
+
